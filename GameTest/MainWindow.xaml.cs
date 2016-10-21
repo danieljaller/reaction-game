f34e03c26d46eb4fb46be6f85f7b7b88cbe60934
Daniel Jaller <danieljaller@gmail.com>
2016-10-21 11:56:03 +0200

    Reaction game with lots of bugs

--- GameTest/MainWindow.xaml.cs
+++ GameTest/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace GameTest
 {
     /// <summary>
@@ -23,6 +26,59 @@ namespace GameTest
         public MainWindow()
         {
             InitializeComponent();
+            Start();
+        }
+        private void Start()
+        {
+            startPage.Visibility = Visibility.Visible;
+            waitForIt.Visibility = Visibility.Hidden;
+            go.Visibility = Visibility.Hidden;
+            displayWinner.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            startPage.Visibility = Visibility.Hidden;
+            waitForIt.Visibility = Visibility.Visible;
+            WaitForGo();
+        }
+        private async void WaitForGo()
+        {
+            var randomizer = new Random();
+            int timeSpan = randomizer.Next(3000, 5000);
+            await Task.Delay(timeSpan);
+            waitForIt.Visibility = Visibility.Hidden;
+            go.Visibility = Visibility.Visible;
+            KeyDown += PressKeyOnGo;            
+        }
+
+        private void PressKeyOnGo(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.A)
+            {
+                DisplayWinner("A");
+            }
+            if (e.Key == Key.L)
+            {
+                DisplayWinner("L");
+            }
+        }
+
+        private void DisplayWinner(string winningPlayer)
+        {
+            go.Visibility = Visibility.Hidden;
+            displayWinner.Visibility = Visibility.Visible;
+            winner.Text = winningPlayer;
+        }
+
+        private void buttonRestart_Click(object sender, RoutedEventArgs e)
+        {
+            Start();
+        }
+
+        private void buttonQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
